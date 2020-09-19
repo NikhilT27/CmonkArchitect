79fce4c39b5bcbdef9fa9fecd0822be15fa771f1
NikhilT27 <nikhilthakare14@gmail.com>
2020-09-19 23:56:52 +0530

    Heroku

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '01e8af0965a92eede092ae7ac3136409'),
+    },
+  },
+});
